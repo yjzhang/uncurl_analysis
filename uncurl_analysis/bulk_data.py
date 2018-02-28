a5aab8c361121253cfa0a87bf1fa29fab5e3c231
Yue Zhang <yjzhang@cs.washington.edu>
2018-02-28 11:48:55 -0800

    added enrichr api

--- uncurl_analysis/bulk_data.py
+++ uncurl_analysis/bulk_data.py
@@ -1,3 +1,60 @@
 # correlations with bulk data
 
 # method for correlating cell types (that's not rank correlation)?
+
+# each bulk dataset defines a categorical distribution over genes (bulk datasets are normalized so that they sum to 1)
+
+# so P(cell | bulk) = P_multinomial(cell | bulk_genes, cell_read_count)
+
+# or... each bulk dataset defines a Poisson distribution?
+
+# P(cell_i | bulk) = P_poisson(cell_i | \lambda=bulk_i*cell_read_count)
+# and assume that each gene is independent...
+# so P(cell | bulk) = \prod_i P_poisson(cell_i | bulk_i*cell_read_count)
+
+# what about comparing a cluster (of single cells) mean to a bulk mean? can we use the same Poisson thing?
+
+# TODO: test classification accuracy of this method vs Euclidean distance, correlation, etc.
+
+# now, what if we have a ton of bulk datasets, and we want to query them? can we do some kinda locality-sensitive hash for probability?
+
+# and how do we do gene matching? we have to have a standard for gene names (an ensembl translator?)
+
+# library of bulk datasets
+
+# alternatively, we can convert cell to a distribution and calculate divergence (but what about presence of zeros? do we just take the nonzeros?)
+
+from scipy.stats import pearsonr, spearmanr
+
+def log_prob_poisson(bulk_data, cell):
+    """
+    Log-probability of cell given bulk, where
+    P(cell | bulk) = \prod_i P_poisson(cell_i | bulk_i*cell_read_count)
+
+    Assumes the same genes in both datasets.
+
+    Args:
+        bulk_data (array): 1d array
+        cell (array): 1d array
+    """
+    cell_read_count = cell.sum()
+    b = bulk_data*cell_read_count
+
+def rank_correlation(bulk_dataset, cell):
+    return spearmanr(bulk_dataset, cell)[0]
+
+def pearson_correlation(bulk_dataset, cell):
+    return pearsonr(bulk_dataset, cell)[0]
+
+def correlation_lookup(datasets, cell, method='poisson'):
+    """
+    Returns a list of (dataset, value) pairs sorted by descending value,
+    where value indicates similarity between the cell and the dataset.
+
+    Potential metrics:
+        - corr/pearson
+        - rank_corr/spearman
+        - cosine (normalized cosine distance)
+        - poisson (log-probability)
+    """
+    pass
--- uncurl_analysis/enrichr_api.py
+++ uncurl_analysis/enrichr_api.py
@@ -5,14 +5,58 @@
 import json
 import requests
 
-ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/addList'
+ENRICHR_URL = 'http://amp.pharm.mssm.edu/Enrichr/'
+ENRICHR_LIBRARIES = [
+        'Human_Gene_Atlas',
+        'Mouse_Gene_Atlas',
+        'Allen_Brain_Atlas_up',
+        'GTEx_Tissue_Sample_Gene_Expression_Profiles_up',
+        'ARCHS4_Tissues',
+        'ARCHS4_Cell-lines',
+        'GO_Biological_Process_2017b',
+        'GO_Cellular_Component_2017b',
+        'GO_Molecular_Function_2017b',
+        ]
 
 def enrichr_add_list(gene_list, description=''):
     """
     Args:
         gene_list (list): a list of gene names
+
+    Returns:
+        user_list_id
     """
+    query_param = 'addList'
     payload = {
             'list': (None, '\n'.join(gene_list)),
             'description': (None, description)
             }
+    response = requests.post(ENRICHR_URL + query_param, files=payload)
+    if not response.ok:
+        raise Exception('Error analyzing gene list')
+    data = json.loads(response.text)
+    user_list_id = data['userListId']
+    short_id = data['shortId']
+    print(data)
+    return user_list_id
+
+def enrichr_query(user_list_id,
+        gene_set_library='Human_Gene_Atlas'):
+    """
+    Args:
+        user_list_id (str): output of enrichr_add_list
+        gene_set_library (str): something in ENRICHR_LIBRARIES, or another Enrichr library - see http://amp.pharm.mssm.edu/Enrichr/#stats
+
+    Returns:
+        list of top terms identified by enrichr, where each term is represented by a list containing: Rank, Term name, P-value, Z-score, Combined score, Overlapping genes, Adjusted p-value
+    """
+    query_param = 'enrich'
+    query_string = '?userListId={0}&backgroundType={1}'
+    response =  requests.get(ENRICHR_URL
+            + query_param
+            + query_string.format(user_list_id, gene_set_library))
+    if not response.ok:
+        raise Exception('Error fetching enrichment results')
+    data = json.loads(response.text)
+    print(data)
+    return data[gene_set_library]
