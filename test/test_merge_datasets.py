aaae360dea3e2ffa9a54ff03c8b507115ae6f95d
Yue Zhang <yjzhang@cs.washington.edu>
2019-11-18 21:04:23 -0800

    merge datasets

---
+++ test/test_merge_datasets.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+import unittest
+
+import numpy as np
+from scipy import sparse
+import scipy.io
+
+from uncurl_analysis import merge_datasets
+
+class MergeTest(unittest.TestCase):
+
+    def setUp(self):
+        # TODO: copy/merge datasets
+        dat = scipy.io.loadmat('data/10x_pooled_400.mat')
+        self.data_dir = '/tmp/uncurl_analysis/test'
+        try:
+            shutil.rmtree(self.data_dir)
+            os.makedirs(self.data_dir)
+        except:
+            os.makedirs(self.data_dir)
+        self.data = sparse.csc_matrix(dat['data'])
+        data_1 = self.data[:, :200]
+        data_2 = self.data[:, 200:]
+        scipy.io.mmwrite(os.path.join(self.data_dir, 'data_1.mtx'), data_1)
+        scipy.io.mmwrite(os.path.join(self.data_dir, 'data_2.mtx'), data_2)
+        data_2_trimmed = data_2[:-10, :]
+        scipy.io.mmwrite(os.path.join(self.data_dir, 'data_3.mtx'), data_2_trimmed)
+        genes = np.loadtxt('data/10x_pooled_400_gene_names.tsv', dtype=str)
+        self.genes = genes
+        genes_trimmed = genes[:-10]
+        print(data_2_trimmed.shape, genes_trimmed.shape)
+        np.savetxt(os.path.join(self.data_dir, 'gene_names_3.txt'), genes_trimmed, fmt='%s')
+        shutil.copy('data/10x_pooled_400_gene_names.tsv', os.path.join(self.data_dir, 'gene_names_1.txt'))
+        shutil.copy('data/10x_pooled_400_gene_names.tsv', os.path.join(self.data_dir, 'gene_names_2.txt'))
+        self.data_paths_1 = [os.path.join(self.data_dir, 'data_1.mtx'), os.path.join(self.data_dir, 'data_2.mtx')]
+        self.data_paths_2 = [os.path.join(self.data_dir, 'data_1.mtx'), os.path.join(self.data_dir, 'data_3.mtx')]
+        self.gene_paths_1 = [os.path.join(self.data_dir, 'gene_names_1.txt'), os.path.join(self.data_dir, 'gene_names_2.txt')]
+        self.gene_paths_2 = [os.path.join(self.data_dir, 'gene_names_1.txt'), os.path.join(self.data_dir, 'gene_names_3.txt')]
+
+    def test_merge_data_1(self):
+        # merge datasets with the same genes
+        data_path, genes_path = merge_datasets.merge_files(self.data_paths_1, self.gene_paths_1, ['0', '1'], self.data_dir)
+        self.assertTrue(data_path.endswith('.mtx.gz'))
+        data = scipy.io.mmread(data_path)
+        genes = np.loadtxt(genes_path, dtype=str)
+        print(data.shape)
+        self.assertEqual(data.shape[1], 400)
+        self.assertEqual(data.shape[0], genes.shape[0])
+        self.assertEqual(data.shape[0], 19848)
+        self.assertTrue((self.data - data).__abs__().sum() < 0.00001)
+        self.assertEqual(set(self.genes), set(genes))
+
+    def test_merge_data_2(self):
+        # merge datasets with the same genes
+        data_path, genes_path = merge_datasets.merge_files(self.data_paths_2, self.gene_paths_2, ['0', '1'], self.data_dir)
+        data = scipy.io.mmread(data_path)
+        genes = np.loadtxt(genes_path, dtype=str)
+        print(data.shape)
+        self.assertEqual(data.shape[1], 400)
+        self.assertEqual(data.shape[0], genes.shape[0])
+        #data_test = []
+        #gene_to_index = {gene : i for i, gene in enumerate(self.genes)}
+        #gene_to_index = {}
+        #for i, g in enumerate(self.genes):
+        #    if g in gene_to_index:
+        #        gene_to_index[g].append(i)
+        #    else:
+        #        gene_to_index[g] = [i]
+        #for gene in genes:
+        #    data_test.append(self.data[gene_to_index[gene], :])
+        #self.data = sparse.vstack(data_test)
+        self.assertEqual(set(self.genes), set(genes))
+        #print((self.data - data).__abs__())
+        #self.assertTrue((self.data - data).__abs__().sum() < 0.00001)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ uncurl_analysis/merge_datasets.py
@@ -0,0 +1,113 @@
+# TODO: functions to merge data matrices
+import os
+import numpy as np
+from scipy import sparse
+import scipy.io
+
+def merge_files(data_paths, gene_paths, dataset_names, output_path):
+    """
+    Merges multiple files into a single file...
+
+    Args:
+        data_paths (list of paths to data files - mtx or txt files, in genes x cells order)
+        gene_paths (list of paths to gene txt files)
+        dataset_names (names of each dataset)
+        output_path (directory to write to)
+
+    This saves an output file as 'data.mtx.gz' in output_path, and a genes file as 'gene_names.txt',
+    and returns two files: a sparse
+
+    It also deletes all the temporary files.
+    """
+    if len(data_paths) == 1 and len(gene_paths) == 1:
+        # TODO: copy files into the correct name
+        gene_output_path = gene_paths[0].replace('_1', '')
+        data_output_path = data_paths[0].replace('_1', '')
+        os.rename(gene_paths[0], gene_output_path)
+        os.rename(data_paths[0], data_output_path)
+        return data_output_path, gene_output_path
+    all_data = []
+    all_genes = []
+    genes_set = set()
+    data_array = []
+    for data_path, gene_path, dataset_name in zip(data_paths, gene_paths, dataset_names):
+        # TODO: if gene_path is None... what do we do?
+        if gene_path is not None:
+            genes = np.loadtxt(gene_path, dtype=str)
+        if data_path.endswith('mtx.gz') or data_path.endswith('mtx'):
+            data = scipy.io.mmread(data_path)
+        else:
+            data = np.loadtxt(data_path)
+        data_array += [dataset_name]*data.shape[1]
+        all_genes.append(genes)
+        genes_set.update(genes)
+        all_data.append(data)
+    # TODO: save data_array?
+    # combine gene lists
+    # decide whether any of the genes are different
+    all_genes_same = True
+    for g1 in all_genes:
+        for g2 in all_genes:
+            if len(g1) != len(g2) or (g1!=g2).any():
+                all_genes_same = False
+                break
+    data_output_path = os.path.join(output_path, 'data.mtx')
+    genes_output_path = os.path.join(output_path, 'gene_names.txt')
+    if all_genes_same:
+        combined_genes = all_genes[0]
+        # combine matrices...
+        # TODO: make sure that transposing the matrices is done before...
+        # we assume that all input matrices are of shape (gene, cell)
+        output_matrix = sparse.hstack(all_data)
+        # save output matrix as mtx.gz
+        scipy.io.mmwrite(data_output_path, output_matrix)
+        import subprocess
+        subprocess.call(['gzip', data_output_path])
+        data_output_path += '.gz'
+        # save gene path
+        os.rename(gene_paths[0], genes_output_path)
+    else:
+        # TODO: what if multiple genes have the same name?
+        # then we just take the max value
+        combined_genes = np.array(list(genes_set))
+        modified_matrices = []
+        # do the mapping...
+        for genes, data in zip(all_genes, all_data):
+            data = sparse.csr_matrix(data)
+            # use bmat - concatenate by rows
+            gene_to_index = {g:i for i, g in enumerate(genes)}
+            #for i, g in enumerate(genes):
+            #    if g in gene_to_index:
+            #        gene_to_index[g].append(i)
+            #    else:
+            #        gene_to_index[g] = [i]
+            sub_blocks = []
+            for gene in combined_genes:
+                if gene not in gene_to_index:
+                    sub_blocks.append([sparse.csr_matrix(np.zeros(data.shape[1]))])
+                else:
+                    sub_blocks.append([data[gene_to_index[gene], :]])
+                    #sub_blocks.append([data[gene_to_index[gene], :].max(0)])
+            data_new = sparse.bmat(sub_blocks)
+            modified_matrices.append(data_new)
+        output_matrix = sparse.hstack(modified_matrices)
+        scipy.io.mmwrite(data_output_path, output_matrix)
+        import subprocess
+        subprocess.call(['gzip', data_output_path])
+        data_output_path += '.gz'
+        # save gene path
+        np.savetxt(genes_output_path, combined_genes, fmt='%s')
+    # remove uploaded files
+    for path in gene_paths:
+        try:
+            os.remove(path)
+        except:
+            pass
+    for path in data_paths:
+        try:
+            os.remove(path)
+        except:
+            pass
+    return data_output_path, genes_output_path
+
+
