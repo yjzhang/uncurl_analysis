9e2cefc6c70dc150732af1709e5e83f3226cd940
Yue Zhang <yjzhang@cs.washington.edu>
2021-10-11 17:09:55 -0700

    updated example so that it works now

--- examples/batch_correction_example.py
+++ examples/batch_correction_example.py
@@ -18,7 +18,8 @@ data1 = data[:, batch1]
 data2 = data[:, batch2]
 data1_dense = data1.T.toarray()
 data2_dense = data2.T.toarray()
-data_corrected = mnnpy.mnn_correct(data1.T, data2.T) 
+var_index = list(range(data1.shape[0]))
+data_corrected = mnnpy.mnn_correct(data1.T, data2.T, var_index=var_index)
 
-data_corrected_dense = mnnpy.mnn_correct(data1_dense, data2_dense)
+data_corrected_dense = mnnpy.mnn_correct(data1_dense, data2_dense, var_index=var_index)
 
